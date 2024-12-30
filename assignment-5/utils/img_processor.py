5664b792f46c75576f04108b2f45401c30331cc3
zkamper <ionut.calin645@gmail.com>
2024-12-30 16:00:55 +0200

    define state

--- assignment-5/utils/img_processor.py
+++ assignment-5/utils/img_processor.py
@@ -1,26 +1,42 @@
 import matplotlib.pyplot as plt
 import cv2
+import numpy as np
+from scipy.ndimage import convolve
+
+def apply_convolution(image):
+    kernel = np.ones((3,3))
+    convolved = convolve(image, kernel)
+    result = np.where(convolved >= 3* 255, 255, image)
+    return result
 
 def process_image(image):
     image = image[20:380, :, :]
     image = cv2.resize(image, (72, 72))
-    blur = cv2.blur(image, (7, 7))
-    pipes = blur[:, :, 0]
-    pipes[pipes > 120] = 255
-    pipes[pipes < 100] = 0
-    bird_mask = image[:, :, 2]
-    bird_mask[bird_mask > 142] = 255
-    bird_mask[bird_mask <= 142] = 0
-    pipes.reshape(72, 72)
-    bird_mask.reshape(72, 72)
-    # plt.imshow(pipes, cmap='gray')
-    # plt.show()
-    # plt.imshow(image[:, :, 1], cmap='gray')
-    # plt.show()
+
+    bird_1 = image[:, :, 0]
+    bird_1[bird_1 > 227] = 255
+    bird_1[bird_1 <= 227] = 0
+
+    bird_2 = image[:, :, 2]
+    bird_2[bird_2 > 138] = 255
+    bird_2[bird_2 <= 138] = 0
+
+    bird = np.zeros((72, 72))
+    bird[(bird_1 == 255) | (bird_2 == 255)] = 255
+    bird_mask = apply_convolution(bird)
+
+    pipes = image[:, :, 1]
+    pipes[pipes > 136] = 255
+    pipes[pipes <=136] = 0
+
+    final_state = pipes
+    final_state = np.where(bird_mask == 255, 127, final_state)
+
     # plt.imshow(bird_mask, cmap='gray')
     # plt.show()
+    # plt.imshow(pipes, cmap='gray')
+    # plt.show()
+    # plt.imshow(final_state, cmap='gray')
+    # plt.show()
 
-    state = image[:, :, 1]
-    state[state > 0] = 255
-    state.reshape(72, 72)
-    return state
+    return final_state
